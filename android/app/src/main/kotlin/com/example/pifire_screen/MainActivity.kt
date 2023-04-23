569741c6416818778719eace4f1133f5e660588e
Tukutt <tukutt@gmail.com>
2023-04-23 02:50:06 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/pifire_screen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pifire_screen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const appTitle = 'Drawer Demo';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: appTitle,
+      home: const MyHomePage(title: appTitle),
+      theme: ThemeData.dark(),
+      // darkTheme: ThemeData.dark(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 40,
+        flexibleSpace: FlexibleSpaceBar(
+          background: Stack(
+            children: [
+              Align(
+                alignment: AlignmentDirectional(0.0, 0.0),
+                child: Container(
+                  width: 200.0,
+                  decoration: BoxDecoration(),
+                  alignment: const AlignmentDirectional(0.0, 0.0),
+                  child: const Text(
+                    'Hello World Milieu',
+                  ),
+                ),
+              ),
+              Align(
+                alignment: const AlignmentDirectional(1.0, 0.0),
+                child: Container(
+                  width: 100.0,
+                  height: 100.0,
+                  decoration: const BoxDecoration(),
+                  alignment: const AlignmentDirectional(0.0, 0.0),
+                  child: const Text(
+                    'Hello World Droite',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        centerTitle: false,
+        elevation: 4.0,
+      ),
+      body: SafeArea(
+        child: Container(),
+      ),
+      drawer: SizedBox(
+        width: 70.0,
+        child: Drawer(
+          elevation: 16.0,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: const EdgeInsetsDirectional.fromSTEB(
+                        0.0, 5.0, 0.0, 10.0),
+                    child: Image.asset(
+                      'assets/images/flame.png',
+                      width: 40.0,
+                      //height: 100.0,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  ListView(
+                    padding: EdgeInsets.zero,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.vertical,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(
+                            0.0, 0.0, 0.0, 0.0),
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.outdoor_grill,
+                            size: 30.0,
+                          ),
+                          onPressed: () {
+                            print('DashboardMenu pressed ...');
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(
+                            0.0, 0.0, 0.0, 0.0),
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.restore,
+                            size: 30.0,
+                          ),
+                          onPressed: () {
+                            print('IconButton pressed ...');
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(
+                            0.0, 0.0, 0.0, 0.0),
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.local_dining,
+                            size: 30.0,
+                          ),
+                          onPressed: () {
+                            print('IconButton pressed ...');
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(
+                            0.0, 0.0, 0.0, 0.0),
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.add,
+                            size: 30.0,
+                          ),
+                          onPressed: () {
+                            print('IconButton pressed ...');
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(
+                            0.0, 0.0, 0.0, 0.0),
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.event_note,
+                            size: 30.0,
+                          ),
+                          onPressed: () {
+                            print('IconButton pressed ...');
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(
+                            0.0, 0.0, 0.0, 0.0),
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.settings,
+                            size: 30.0,
+                          ),
+                          onPressed: () {
+                            print('IconButton pressed ...');
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
